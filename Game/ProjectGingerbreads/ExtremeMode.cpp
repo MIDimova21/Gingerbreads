7658aaf19c676227cda4a5908f372cbb8ab0e2c8
MVKoleva21 <MVKoleva21@codingburgas.bg>
2022-12-08 00:55:26 +0200

    Add ExtremeMode words & generator

--- Game/ProjectGingerbreads/ExtremeMode.cpp
+++ Game/ProjectGingerbreads/ExtremeMode.cpp
@@ -3,3 +3,25 @@
 #include<time.h> // time
 #include<cstdlib> // rand, srand
 using namespace std;
+
+void ExtremeGenerator()
+{
+	// seeds random number
+	srand(time(NULL));
+	string extrememodewords[30] = {
+		"snazzy", "avatar", "diadem", "guitar", "miacid",
+		"ocelot", "burgle", "gnomic", "clutch", "schist",
+		"callow", "gneiss", "panzer", "harbor", "mohawk",
+		"anoxia", "quahog", "slithy", "proton", "nettly",
+		"velvet", "whinge", "gerbil", "harbor", "bathos",
+		"tattoo", "acetic", "python", "xystis", "dengue"
+	};
+
+	// generates the index of the word in the array and its variable and saves the word in a new variable
+	int index = rand() % 30;
+	string word = extrememodewords[index];
+
+	string totable[1][4] = {
+		//in incoming commit
+	}
+}
