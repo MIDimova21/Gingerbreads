6be4c8c5208451162939336baf3d3350210dc774
MVKoleva21 <MVKoleva21@codingburgas.bg>
2022-12-12 00:41:33 +0200

    Update menu

--- Game/ProjectGingerbreads/Menu.cpp
+++ Game/ProjectGingerbreads/Menu.cpp
@@ -3,26 +3,21 @@
 #include "../ProjectGingerbreads/EasyMode.h"
 #include "../ProjectGingerbreads/AdvancedMode.h"
 #include "../ProjectGingerbreads/ExtremeMode.h"
-#include "../ProjectGingerbreads/ChristmasEdition.h"
 #include<iostream>
 #include<iomanip>
 using namespace std;
 
 void displaymenu()
 {
-	system("cls");
-	cout << "=================================================================" << endl;
-	cout << "|                                                               |" << endl;
-	cout << "|   $$\\      $$\\                           $$\\ $$\\              |" << endl;
-	cout << "|   $$ | $\\  $$ |                          $$ |$$ |             |" << endl;
-	cout << "|   $$ |$$$\\ $$ | $$$$$$\\   $$$$$$\\   $$$$$$$ |$$ | $$$$$$\\     |" << endl;
-	cout << "|   $$ $$ $$\\$$ |$$  __$$\\ $$  __$$\\ $$  __$$ |$$ |$$  __$$\\    |" << endl;
-	cout << "|   $$$$  _$$$$ |$$ /  $$ |$$ |  \\__|$$ /  $$ |$$ |$$$$$$$$ |   |" << endl;
-	cout << "|   $$$  / \\$$$ |$$ |  $$ |$$ |      $$ |  $$ |$$ |$$   ____|   |" << endl;
-	cout << "|   $$  /   \\$$ |\\$$$$$$  |$$ |      \\$$$$$$$ |$$ |\\$$$$$$$\\    |" << endl;
-	cout << "|   \\__/     \\__| \\______/ \\__|       \\_______|\\__| \\_______|   |" << endl;
-	cout << "|                                                               |" << endl;
-	cout << "=================================================================" << endl;
+	cout << "                                                               " << endl;
+	cout << "   $$\\      $$\\                           $$\\    $$\\              " << endl;
+	cout << "   $$ | $\\  $$ |                          $$\\   |$$ |             " << endl;
+	cout << "   $$ |$$$\\ $$ | $$$$$$\\   $$$$$$\\   $$$$$$$\\   |$$ | $$$$$$\\     " << endl;
+	cout << "   $$ $$ $$\\$$ |$$  __$$\\ $$  __$$\\ $$  ___$$\\  |$$ |$$  __$$\\    " << endl;
+	cout << "   $$$$  _$$$$ |$$ /  $$ |$$ |  \\__|$$ /   |$$\\ |$$ |$$$$$$$$ |   " << endl;
+	cout << "   $$$  / \\$$$ |$$ |  $$ |$$ | |    $$ |   |$$\\ |$$ |$$   ____|   " << endl;
+	cout << "   $$   /   \\$$ |\\$$$$$$  |$$ |      \\$$$$$$$$\\ |$$ |\\$$$$$$$\\    " << endl;
+	cout << "   \\__/     \\__| \\______/ \\__|       \\____\\___|\\____| \\_______|   \n" << endl;
 }
 
 void start()
@@ -34,7 +29,6 @@ void start()
 	cout << "1. Easy Mode" << endl;
 	cout << "2. Advanced Mode" << endl;
 	cout << "3. Extreme Mode" << endl;
-	cout << "4. Christmas Edition" << endl;
 	do
 	{
 		cout << "\nEnter your choice:\n";
@@ -43,37 +37,31 @@ void start()
 		cin >> confirm;
 		switch (yourchoice)
 		{
-		case 1:
-		{
-			ClearScreen();
-			EasyMode();
-			break;
-		}
-		case 2:
-		{
-			ClearScreen();
-			AdvancedMode();
-			break;
-		}
-		case 3:
-		{
-			ClearScreen();
-			ExtremeMode();
-			break;
-		}
-		case 4:
-		{
-			ClearScreen();
-			ChristmasEdition();
-			break;
-		}
-		default:
-		{
-			ClearScreen();
-			displaymenu();
-			cout << "\nYou have entered an invalid option, please enter a valid one!\n";
-			break;
-		}
+			case 1:
+			{
+				ClearScreenMenu();
+				EasyMode();
+				break;
+			}
+			case 2:
+			{
+				ClearScreenMenu();
+				AdvancedMode();
+				break;
+			}
+			case 3:
+			{
+				ClearScreenMenu();
+				ExtremeMode();
+				break;
+			}
+			default:
+			{
+				ClearScreenMenu();
+				cout << "\nYou have entered an invalid option, please enter a valid one!\n";
+				start();
+				break;
+			}
 		}
 	} while (confirm == "y" || confirm == "Y");
 }
